d596432eb31b7b82117c5ed0bcc0c8be8e03d8a8
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-06-25 16:50:18 +0200

    components Finale/Platz3

---
+++ src/components/Finale/index.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import FlagSet from '../FlagSet'
+import { localeName as locales } from '../countries'
+import styles from "./index.module.css"
+
+type Match = {
+  teams: string[] | string[][]
+  goals: (number | null)[]
+  add: string | null
+  date: string
+}
+
+type Props = {
+  match: Match
+}
+
+function Finale({ match }: Props) {
+  return (
+    <div className={styles.matchWrapper}>
+      <h3 className={styles.title}>
+        {locales("Final")}
+      </h3>
+      <table className={styles.table}>
+        <tbody>
+          <tr>
+            <FinaleSingle match={match} />
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+function Platz3({ match }: Props) {
+  return (
+    <div className={styles.matchWrapper}>
+      <h3 className={styles.title}>
+        {locales("Place3")}
+      </h3>
+      <table className={styles.table}>
+        <tbody>
+          <tr>
+            <FinaleSingle match={match} />
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+function FinaleSingle({ match }: Props) {
+  const { teams, goals, add, date } = match
+  const tz = Intl.DateTimeFormat().resolvedOptions().timeZone
+  const formattedDate = date ? new Date(date).toLocaleString(navigator.language, { year: "2-digit", month: "2-digit", day: "2-digit", hour: "2-digit", minute: "2-digit", timeZone: tz }) : null
+  return (
+    <React.Fragment>
+      <td className={styles.team}><FlagSet code={teams[0]} /></td>
+      <td className={styles.team}><FlagSet code={teams[1]} /></td>
+      {teams[0] && teams[1] && typeof goals[0] === 'number' && typeof goals[1] === 'number' ? (<td className={styles.result}>{`${typeof goals[0] === "number" ? goals[0] : "-"}:${typeof goals[1] === "number" ? goals[1] : "-"} ${add ? add : ""}`}</td>) : formattedDate ? (<td className={styles.result}>{formattedDate}</td>) : (<td className={styles.result}>-:-</td>)}
+    </React.Fragment>
+  )
+}
+
+export { Platz3, Finale }
