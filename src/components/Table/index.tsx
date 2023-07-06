8eefb8fcb344860270b6ad40dd93b0f4973363a3
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-07-06 21:22:02 +0200

    add Table components

---
+++ src/components/Table/index.tsx
@@ -0,0 +1,200 @@
+'use client'
+import React, { useEffect } from 'react'
+import { FlagWrapper } from "../FlagSet"
+import { participantName as participants, localeName as locales } from '../countries'
+import { getGridValue } from '../helpers'
+import styles from './index.module.css'
+/* eslint-disable react-hooks/exhaustive-deps */
+
+type Dataset = {
+  team: string
+  points: number
+  goals: number
+  countergoals: number
+  goalDifference: number
+  fairPlay: number
+  victories: number
+}
+
+type Dataset3 = {
+  team: string
+  points: number
+  goals: number
+  countergoals: number
+  goalDifference: number
+  fairPlay: number
+  victories: number
+  group: string
+}
+
+type TableProps = {
+  table: Dataset[]
+  number: number
+  notifier: string
+}
+
+type TableThirdProps = {
+  third: Dataset3[]
+  emax?: number
+  maxColumns?: number
+}
+
+type Group = {
+  table: Dataset[]
+}
+
+type TableSetProps = {
+  matches: { [key: string]: Group }
+}
+
+export function GeneralTableSet({ matches }: TableSetProps) {
+  const keys = Object.keys(matches)
+  const sortedKeys = keys.sort()
+  const sortedValues = sortedKeys.map(key => matches[key])
+  const gridValue = getGridValue(keys.length)
+  let className = styles.grid_2
+  switch (gridValue) {
+    case 1:
+      className = styles.grid_1 
+      break
+    case 3:
+      className = styles.grid_3 
+      break
+    case 4:
+      className = styles.grid_4 
+      break 
+    default:
+      className = styles.grid_2
+  }
+  if (sortedValues.length > 0) {
+    return (
+      <div className={className}>
+        {sortedValues.map((group, index) => (<NewTable table={group.table} number={index} key={index} notifier={sortedKeys[index]} />))}
+      </div>
+    )
+  }
+  return null
+}
+
+function NewTable ({ table, number, notifier }: TableProps) {
+  let className = styles.tablemain
+  switch (number) {
+    case 0:
+      className = styles.table_0
+      break
+    case 1:
+      className = styles.table_1 
+      break
+    case 2:
+      className = styles.table_2 
+      break
+    case 3:
+      className = styles.table_3 
+      break
+    case 4:
+      className = styles.table_4 
+      break
+    case 5:
+      className = styles.table_5 
+      break
+    case 6:
+      className = styles.table_6 
+      break
+    case 7:
+      className = styles.table_7 
+      break 
+    default:
+      className = styles.tablemain
+  }
+  if (table.length > 0) {
+    return (
+    <div className={className}>
+      <div className={styles.notifier}>{notifier}</div>
+      <table className={styles.table}>
+        <thead>
+          <tr className={styles.tablerow}>
+            <td>{locales("Team")}</td>
+            <td>{locales("Pts")}</td>
+            <td>{locales("Difference")}</td>
+            <td>{locales("Goals")}</td>
+            <td>{locales("FairPlay")}</td>
+          </tr>
+        </thead>
+        <tbody>
+          {table.map((dataset: Dataset, index: number) => {
+            const { team, points, goals, countergoals, goalDifference, fairPlay } = dataset
+            return (
+              <tr key={index} className={styles.tablerow}>
+                <td><FlagWrapper team={team} participant={participants(team)} /></td>
+                <td>{points}</td>
+                <td>{goalDifference}</td>
+                <td>{goals}:{countergoals}</td>
+                <td>{fairPlay}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
+    )
+  }
+  return null
+}
+
+export function TableThird({ third, emax, maxColumns }: TableThirdProps) {
+  const max = emax ? emax : 3
+  let tableclass = styles.tablethird_2
+  switch (maxColumns) {
+    case 3:
+      tableclass = styles.tablethird_3 
+      break
+    case 4:
+      tableclass = styles.tablethird_4 
+      break 
+    case 5:
+      tableclass = styles.tablethird_5 
+      break 
+    case 6:
+      tableclass = styles.tablethird_6 
+      break
+    case 7:
+      tableclass = styles.tablethird_7 
+      break
+    case 8:
+      tableclass = styles.tablethird_8 
+      break 
+    default:
+      tableclass = styles.tablethird_2
+  }
+  return (
+    <table className={tableclass}>
+      <thead>
+        <tr>
+        <td>{locales("Group")}</td>
+        <td>{locales("Team")}</td>
+        <td>{locales("Pts")}</td>
+        <td>{locales("Victories")}</td>
+        <td>{locales("Difference")}</td>
+        <td>{locales("FairPlay")}</td>
+        </tr>
+      </thead>
+      <tbody>
+        {third && third.length > 0 && third.map((team: Dataset3, index: number) => {
+          const line = index === max ? styles.border : styles.noborder
+          if (team) {
+            return (
+              <tr key={index}>
+                <td className={line}>{team.group}</td>
+                <td className={line}><FlagWrapper team={team.team} participant={participants(team.team)} /></td>
+                <td className={line}>{team.points}</td>
+                <td className={line}>{team.victories}</td>
+                <td className={line}>{team.goalDifference}</td>
+                <td className={line}>{team.fairPlay}</td>
+              </tr>
+            )
+          } else { return null }
+        })}
+      </tbody>
+    </table>
+  )
+}
