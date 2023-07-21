d5c6fd2de9c6043c33ff6e8cea7c52c8ee1db144
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-07-21 16:20:09 +0200

    remove missing links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,64 +5,64 @@ import { localeName as locales } from '@/components/countries'
 import Link from 'next/link'
 
 const tournaments = [
-  {
-    title: `${locales("EuroMen")}`,
-    editions: [
-      {
-        link: "/euromen/2020",
-        title: `${locales("Euro")} 2020`
-      },
-      {
-        link: "/euromen/2016",
-        title: `${locales("Euro")} 2016`
-      },
-      {
-        link: "/euromen/2012",
-        title: `${locales("Euro")} 2012`
-      },
-      {
-        link: "/euromen/2008",
-        title: `${locales("Euro")} 2008`
-      },
-      {
-        link: "/euromen/2004",
-        title: `${locales("Euro")} 2004`
-      },
-    ],
-  },
-  {
-    title: `${locales("EuroWomen")}`,
-    editions: [
-      {
-        link: "/eurowomen/2022",
-        title: `${locales("Euro")} 2022`
-      },
-      {
-        link: "/eurowomen/2017",
-        title: `${locales("Euro")} 2017`
-      },
-      {
-        link: "/eurowomen/2013",
-        title: `${locales("Euro")} 2013`
-      },
-      {
-        link: "/eurowomen/2009",
-        title: `${locales("Euro")} 2009`
-      },
-      {
-        link: "/eurowomen/2005",
-        title: `${locales("Euro")} 2005`
-      },
-      {
-        link: "/eurowomen/2001",
-        title: `${locales("Euro")} 2001`
-      },
-      {
-        link: "/eurowomen/1997",
-        title: `${locales("Euro")} 1997`
-      }
-    ],
-  },
+  // {
+  //   title: `${locales("EuroMen")}`,
+  //   editions: [
+  //     {
+  //       link: "/euromen/2020",
+  //       title: `${locales("Euro")} 2020`
+  //     },
+  //     {
+  //       link: "/euromen/2016",
+  //       title: `${locales("Euro")} 2016`
+  //     },
+  //     {
+  //       link: "/euromen/2012",
+  //       title: `${locales("Euro")} 2012`
+  //     },
+  //     {
+  //       link: "/euromen/2008",
+  //       title: `${locales("Euro")} 2008`
+  //     },
+  //     {
+  //       link: "/euromen/2004",
+  //       title: `${locales("Euro")} 2004`
+  //     },
+  //   ],
+  // },
+  // {
+  //   title: `${locales("EuroWomen")}`,
+  //   editions: [
+  //     {
+  //       link: "/eurowomen/2022",
+  //       title: `${locales("Euro")} 2022`
+  //     },
+  //     {
+  //       link: "/eurowomen/2017",
+  //       title: `${locales("Euro")} 2017`
+  //     },
+  //     {
+  //       link: "/eurowomen/2013",
+  //       title: `${locales("Euro")} 2013`
+  //     },
+  //     {
+  //       link: "/eurowomen/2009",
+  //       title: `${locales("Euro")} 2009`
+  //     },
+  //     {
+  //       link: "/eurowomen/2005",
+  //       title: `${locales("Euro")} 2005`
+  //     },
+  //     {
+  //       link: "/eurowomen/2001",
+  //       title: `${locales("Euro")} 2001`
+  //     },
+  //     {
+  //       link: "/eurowomen/1997",
+  //       title: `${locales("Euro")} 1997`
+  //     }
+  //   ],
+  // },
   {
     title: `${locales("WCMen")}`,
     editions: [
@@ -89,19 +89,19 @@ const tournaments = [
       {
         link: "/worldcupmen/2002",
         title: `${locales("WorldCup")} 2002`
-      },
-      {
-        link: "/worldcupmen/1998",
-        title: `${locales("WorldCup")} 1998`
-      },
-      {
-        link: "/worldcupmen/1994",
-        title: `${locales("WorldCup")} 1994`
-      },
-      {
-        link: "/worldcupmen/1990",
-        title: `${locales("WorldCup")} 1990`
       }
+      // {
+      //   link: "/worldcupmen/1998",
+      //   title: `${locales("WorldCup")} 1998`
+      // },
+      // {
+      //   link: "/worldcupmen/1994",
+      //   title: `${locales("WorldCup")} 1994`
+      // },
+      // {
+      //   link: "/worldcupmen/1990",
+      //   title: `${locales("WorldCup")} 1990`
+      // }
     ],
   },
   {
@@ -122,27 +122,27 @@ const tournaments = [
       {
         link: "/worldcupwomen/2011",
         title: `${locales("WorldCup")} 2011`
-      },
-      {
-        link: "/worldcupwomen/2007",
-        title: `${locales("WorldCup")} 2007`
-      },
-      {
-        link: "/worldcupwomen/2003",
-        title: `${locales("WorldCup")} 2003`
-      },
-      {
-        link: "/worldcupwomen/1999",
-        title: `${locales("WorldCup")} 1999`
-      },
-      {
-        link: "/worldcupwomen/1995",
-        title: `${locales("WorldCup")} 1995`
-      },
-      {
-        link: "/worldcupwomen/1991",
-        title: `${locales("WorldCup")} 1991`
       }
+      // {
+      //   link: "/worldcupwomen/2007",
+      //   title: `${locales("WorldCup")} 2007`
+      // },
+      // {
+      //   link: "/worldcupwomen/2003",
+      //   title: `${locales("WorldCup")} 2003`
+      // },
+      // {
+      //   link: "/worldcupwomen/1999",
+      //   title: `${locales("WorldCup")} 1999`
+      // },
+      // {
+      //   link: "/worldcupwomen/1995",
+      //   title: `${locales("WorldCup")} 1995`
+      // },
+      // {
+      //   link: "/worldcupwomen/1991",
+      //   title: `${locales("WorldCup")} 1991`
+      // }
     ],
   }
 ]
